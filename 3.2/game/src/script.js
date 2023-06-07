b1477bcc0f3f52b9431a9680735caaf1b86ebbb6
vakabunga <shesterov@gmail.com>
2023-06-08 01:37:52 +0600

    rework game.js and remove settimeout from 2000.html

--- 3.2/game/src/script.js
+++ 3.2/game/src/script.js
@@ -1,90 +1,94 @@
+/* eslint-disable space-before-function-paren */
 /* eslint-disable semi */
-const gameButtons = document.querySelectorAll('.button');
 
-function onClick ({ target }) {
-  if (target.classList.contains('game-button')) {
-    location.assign('./start.html');
-    return;
-  }
-
-  if (target.classList.contains('unwanted-button')) {
-    location.assign('./nextstep.html');
-    return;
-  }
-
-  if (target.classList.contains('boom-button')) {
-    location.assign('./boom.html');
-    return;
-  }
-
-  if (target.classList.contains('wall-button')) {
-    location.assign('./wall.html');
-    return;
-  }
-
-  if (target.classList.contains('bus-station-button')) {
-    location.assign('./bus_station');
-    return;
-  }
-
-  if (target.classList.contains('door-button')) {
-    location.assign('./door');
-    return;
-  }
-
-  if (target.classList.contains('road-button')) {
-    location.assign('./road.html');
-    return;
-  }
-
-  if (target.classList.contains('black-button')) {
-    location.assign('./black.html');
-    return;
-  }
-
-  if (target.classList.contains('shedule-button')) {
-    location.assign('./shedule.html');
-    return;
-  }
-
-  if (target.classList.contains('sit-button')) {
-    location.assign('./sit.html');
-    return;
-  }
-
-  if (target.classList.contains('stay-button')) {
-    location.assign('./stay.html');
-    return;
-  }
-
-  if (target.classList.contains('stay-again-button')) {
-    location.assign('./stay_again.html');
-    return;
-  }
-
-  if (target.classList.contains('lay-button')) {
-    location.assign('./lay.html');
-    return;
-  }
-
-  if (target.classList.contains('sub-wall-button')) {
-    location.assign('./../wall.html');
-    return;
-  }
-
-  if (target.classList.contains('sub-bus-station-button')) {
-    location.assign('./../bus_station');
-    return;
-  }
-
-  if (target.classList.contains('sub-road-button')) {
-    location.assign('./../road.html');
-    return;
-  }
-
-  if (target.classList.contains('sub-game-button')) {
-    location.assign('./../start.html');
-  }
+function getGameButtonClickHandler(url) {
+  return location.assign(url);
 }
 
-gameButtons.forEach(element => element.addEventListener('click', onClick));
+const gameButtons = document.querySelectorAll('.game-button');
+const unwantedButton = document.querySelector('.unwanted-button');
+const boomButton = document.querySelector('.boom-button');
+const wallButton = document.querySelector('.wall-button');
+const busStationButton = document.querySelector('.bus-station-button');
+const doorButton = document.querySelector('.door-button');
+const roadButton = document.querySelector('.road-button');
+const blackButton = document.querySelector('.black-button');
+const sheduleButton = document.querySelector('.shedule-button');
+const sitButton = document.querySelector('.sit-button');
+const stayButton = document.querySelector('.stay-button');
+const stayAgainButton = document.querySelector('.stay-again-button');
+const layButton = document.querySelector('.lay-button');
+const subWallButton = document.querySelector('.sub-wall-button');
+const subBusStationButton = document.querySelector('.sub-bus-station-button');
+const subRoadButton = document.querySelector('.sub-road-button');
+const subGameButton = document.querySelector('.sub-game-button');
+
+gameButtons.forEach(element => {
+  element.addEventListener('click', () => {
+    getGameButtonClickHandler('./start.html');
+  });
+})
+
+unwantedButton.addEventListener('click', function () {
+  getGameButtonClickHandler('./nextstep.html');
+});
+
+boomButton.addEventListener('click', function () {
+  getGameButtonClickHandler('./boom.html');
+});
+
+wallButton.addEventListener('click', function () {
+  getGameButtonClickHandler('./wall.html');
+});
+
+busStationButton.addEventListener('click', function () {
+  getGameButtonClickHandler('./bus_station');
+});
+
+doorButton.addEventListener('click', function () {
+  getGameButtonClickHandler('./door');
+});
+
+roadButton.addEventListener('click', function () {
+  getGameButtonClickHandler('./road');
+});
+
+blackButton.addEventListener('click', function () {
+  getGameButtonClickHandler('./black.html');
+});
+
+sheduleButton.addEventListener('click', function () {
+  getGameButtonClickHandler('./shedule.html');
+});
+
+sitButton.addEventListener('click', function () {
+  getGameButtonClickHandler('./sit.html');
+});
+
+stayButton.addEventListener('click', function () {
+  getGameButtonClickHandler('./stay.html');
+});
+
+stayAgainButton.addEventListener('click', function () {
+  getGameButtonClickHandler('./stay_again.html');
+});
+
+layButton.addEventListener('click', function () {
+  getGameButtonClickHandler('./lay.html');
+});
+
+subWallButton.addEventListener('click', function () {
+  getGameButtonClickHandler('./../wall.html');
+});
+
+subBusStationButton.addEventListener('click', function () {
+  getGameButtonClickHandler('./../bus_station');
+});
+
+subRoadButton.addEventListener('click', function () {
+  getGameButtonClickHandler('./../road.html');
+});
+
+subGameButton.addEventListener('click', function () {
+  getGameButtonClickHandler('./../start.html');
+});
